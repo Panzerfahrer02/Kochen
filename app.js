db854896d680124aef8d166ea2460740bf278341
Panzerfahrer02 <martinlee.mls@gmail.com>
2026-03-04 13:55:09 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+let recipes = [
+{
+name:"Pasta Tomate",
+baseServings:2,
+time:"20 min",
+rating:3,
+ingredients:[
+{name:"Nudeln", amount:200, unit:"g"},
+{name:"Tomaten", amount:2, unit:"pcs"},
+{name:"Knoblauch", amount:1, unit:"pcs"}
+],
+instructions:"Alles kochen und mischen."
+}
+]
+
+let shoppingList = []
+
+let selectedRecipe = null
